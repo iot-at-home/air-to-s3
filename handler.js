86f2b129f1f346fdac8442b5e5d7561115f39892
Sebastian Felix Schwarz <schwarz@nexus-ion.ai>
2019-05-15 22:31:50 +0200

    add function to write on Blynkk LED

--- handler.js
+++ handler.js
@@ -2,9 +2,28 @@
 const AWS = require('aws-sdk');
 const moment = require('moment-timezone');
 const s3 = new AWS.S3({region: 'eu-central-1'});
-
+const axios = require('axios');
 
 const BUCKET = process.env.BUCKET;
+const BLYNK = process.env.BLYNK;
+
+
+async function writePin(pin,val) {
+  try {
+    const response = await axios.get('http://blynk-cloud.com/'+BLYNK+'/update/'+pin+'?value='+val);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+function mapUser2Pin(dev){
+    switch (dev) {
+      case "Hellboy": return "V1";
+      case "Nora": return "V3";
+      case "Guest1": return "V5";
+      case "Guest2": return "V7";
+    }
+}
 
 
 module.exports.athome = async (event) => {
@@ -20,8 +39,10 @@ module.exports.athome = async (event) => {
     Key: filename
   }).promise();
   let user = JSON.parse(data.Body.toString());
-  if(user.active !== obj.active){
+  if(user.active !== obj.active || user.hasOwnProperty('last') === false){
     obj.last = user.update;
+  }else{
+    obj.last = user.last;
   }
 
 
@@ -35,7 +56,7 @@ module.exports.athome = async (event) => {
   };
 
   const key = await s3.upload(s3params).promise();
-
+  const blynk = await writePin(mapUser2Pin(obj.device),obj.last);
   return {
     statusCode: 200,
     body: JSON.stringify({
